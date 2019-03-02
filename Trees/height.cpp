7395b34068bf1106901fd70152fa971e404e6773
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-03-02 20:30:59 +0530

    Create height.cpp

---
+++ Trees/height.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+    
+};
+
+
+#include <queue>
+int height(TreeNode<int>* root) {
+  int h=0;
+  for(int i=0;i<root->children.size();i++){
+    int h1=height(root->children[i]);
+    if(h1>h){
+      h=h1;
+    }
+  }
+	return h+1;
+}
+
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    
+    queue<TreeNode<int>*> pendingNodes;
+    
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << height(root) << endl;
+}
